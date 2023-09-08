ca18cfa8af1c8bd16f9d1412f0bc571b87f0e530
Catherine <sunny4evil@gmail.com>
2023-09-08 19:43:30 +0300

    add test method format to LocalDateFormatterTest

--- src/test/java/com/dmdev/util/LocalDateFormatterTest.java
+++ src/test/java/com/dmdev/util/LocalDateFormatterTest.java
@@ -1,7 +1,21 @@
 package com.dmdev.util;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class LocalDateFormatterTest {
+    @Test
+    void format() {
+    String date = "1234-12-12";
+
+    LocalDate actualResult = LocalDateFormatter.format(date);
+
+        assertThat(actualResult).isEqualTo(LocalDate.of(1234,12,12));
+    }
+
+
 
 }
