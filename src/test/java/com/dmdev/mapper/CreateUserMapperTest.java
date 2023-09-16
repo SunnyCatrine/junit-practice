b6b8e908598e5aa46dcf86a5eb3a08fce2ab94e8
Catherine <sunny4evil@gmail.com>
2023-09-16 18:18:56 +0300

    create test class createUserMapperTest

---
+++ src/test/java/com/dmdev/mapper/CreateUserMapperTest.java
@@ -0,0 +1,12 @@
+package com.dmdev.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreateUserMapperTest {
+
+    @Test
+    void map() {
+    }
+}
