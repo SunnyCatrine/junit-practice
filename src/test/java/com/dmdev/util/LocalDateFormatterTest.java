2fa999d8dc16e1ac0317915c4176a92c988c8f10
Catherine <sunny4evil@gmail.com>
2023-09-16 16:49:26 +0300

    add test method shouldThrowException to LocalDateFormatterTest

--- src/test/java/com/dmdev/util/LocalDateFormatterTest.java
+++ src/test/java/com/dmdev/util/LocalDateFormatterTest.java
@@ -1,18 +1,28 @@
 package com.dmdev.util;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class LocalDateFormatterTest {
     @Test
     void format() {
-    String date = "1234-12-12";
+        String date = "1234-12-12";
 
-    LocalDate actualResult = LocalDateFormatter.format(date);
+        LocalDate actualResult = LocalDateFormatter.format(date);
 
-    assertThat(actualResult).isEqualTo(LocalDate.of(1234,12,12));
+        assertThat(actualResult).isEqualTo(LocalDate.of(1234,12,12));
+    }
+
+    @Test
+    void shouldThrowExceptionIfDateInvalid() {
+        String date = "1234-12-1222";
+
+        assertThrows(DateTimeParseException.class, () -> LocalDateFormatter.format(date));
     }
 }
