9b00f0a3291c4c39ce9ed88c80948a215ef2d2ed
Catherine <sunny4evil@gmail.com>
2023-09-16 18:56:37 +0300

    add loginSucces test method

--- src/test/java/com/dmdev/service/UserServiceTest.java
+++ src/test/java/com/dmdev/service/UserServiceTest.java
@@ -1,16 +1,27 @@
 package com.dmdev.service;
 
 import com.dmdev.dao.UserDao;
+import com.dmdev.dto.CreateUserDto;
+import com.dmdev.dto.UserDto;
+import com.dmdev.entity.Gender;
+import com.dmdev.entity.Role;
+import com.dmdev.entity.User;
 import com.dmdev.mapper.CreateUserMapper;
 import com.dmdev.mapper.UserMapper;
 import com.dmdev.validator.CreateUserValidator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -27,5 +38,31 @@ class UserServiceTest {
 
     @Test
     void loginSuccess() {
+        User user = User.builder()
+                .birthday(LocalDate.of(1234,12,12))
+                .email("123@qwe.com")
+                .gender(Gender.MALE)
+                .name("Mesut")
+                .role(Role.USER)
+                .id(10)
+                .password("qwe")
+                .build();
+
+        UserDto userDto = UserDto.builder()
+                .birthday(LocalDate.of(1234,12,12))
+                .email("123@qwe.com")
+                .gender(Gender.MALE)
+                .name("Mesut")
+                .role(Role.USER)
+                .id(10)
+                .build();
+
+        doReturn(Optional.of(user)).when(userDao).findByEmailAndPassword(user.getEmail(),user.getPassword());
+        doReturn(userDto).when(userMapper).map(user);
+
+        Optional<UserDto> actualResult = userService.login(user.getEmail(), user.getPassword());
+
+        assertThat(actualResult).isPresent();
+        assertThat(actualResult).hasValue(userDto);
     }
 }
