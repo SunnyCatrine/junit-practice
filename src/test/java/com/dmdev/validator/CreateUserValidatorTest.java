d2bbe42a55932c6a256af1d29f1562b1ee01bf5a
Catherine <sunny4evil@gmail.com>
2023-09-16 17:55:07 +0300

    add test method passValidation

--- src/test/java/com/dmdev/validator/CreateUserValidatorTest.java
+++ src/test/java/com/dmdev/validator/CreateUserValidatorTest.java
@@ -1,7 +1,31 @@
 package com.dmdev.validator;
 
+import com.dmdev.dto.CreateUserDto;
+import com.dmdev.entity.Gender;
+import com.dmdev.entity.Role;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateUserValidatorTest {
+
+    CreateUserValidator validator = CreateUserValidator.getInstance();
+
+    @Test
+    void shouldPassValidation() {
+        CreateUserDto createUserDto = CreateUserDto.builder()
+                .birthday("1234-12-12")
+                .email("123@qwe.com")
+                .gender(Gender.MALE.name())
+                .name("Mesut")
+                .password("mesosh")
+                .role(Role.USER.name())
+                .build();
+
+        ValidationResult actualResult = validator.validate(createUserDto);
+
+        assertFalse(actualResult.hasErrors());
+
+    }
 
 }
