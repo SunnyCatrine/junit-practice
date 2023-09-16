cd5cebedc5f35c7154c180c43ab6dc3708a86187
Catherine <sunny4evil@gmail.com>
2023-09-16 18:03:01 +0300

    add test method invalidBirthday

--- src/test/java/com/dmdev/validator/CreateUserValidatorTest.java
+++ src/test/java/com/dmdev/validator/CreateUserValidatorTest.java
@@ -3,13 +3,15 @@ package com.dmdev.validator;
 import com.dmdev.dto.CreateUserDto;
 import com.dmdev.entity.Gender;
 import com.dmdev.entity.Role;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CreateUserValidatorTest {
 
-    CreateUserValidator validator = CreateUserValidator.getInstance();
+    private final CreateUserValidator validator = CreateUserValidator.getInstance();
 
     @Test
     void shouldPassValidation() {
@@ -25,7 +27,23 @@ class CreateUserValidatorTest {
         ValidationResult actualResult = validator.validate(createUserDto);
 
         assertFalse(actualResult.hasErrors());
+    }
 
+    @Test
+    void invalidBirthday() {
+        CreateUserDto createUserDto = CreateUserDto.builder()
+                .birthday("1234-12-1212212")
+                .email("123@qwe.com")
+                .gender(Gender.MALE.name())
+                .name("Mesut")
+                .password("mesosh")
+                .role(Role.USER.name())
+                .build();
+
+        ValidationResult actualResult = validator.validate(createUserDto);
+
+        assertThat(actualResult.getErrors()).hasSize(1);
+        assertThat(actualResult.getErrors().get(0).getCode()).isEqualTo("invalid.birthday");
     }
 
 }
