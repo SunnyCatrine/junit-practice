270a4e232a2eb93a65e81aa7de46d0e5f93f5cb6
Catherine <sunny4evil@gmail.com>
2023-09-16 18:38:43 +0300

    create UserServiceTest class

---
+++ src/test/java/com/dmdev/service/UserServiceTest.java
@@ -0,0 +1,31 @@
+package com.dmdev.service;
+
+import com.dmdev.dao.UserDao;
+import com.dmdev.mapper.CreateUserMapper;
+import com.dmdev.mapper.UserMapper;
+import com.dmdev.validator.CreateUserValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+    @Mock
+    private CreateUserValidator createUserValidator;
+    @Mock
+    private UserDao userDao;
+    @Mock
+    private CreateUserMapper createUserMapper;
+    @Mock
+    private UserMapper userMapper;
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void loginSuccess() {
+    }
+}
