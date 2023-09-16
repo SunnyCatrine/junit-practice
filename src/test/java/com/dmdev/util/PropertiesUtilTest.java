29172f9be2b229b9531b08fca60660bc301797da
Catherine <sunny4evil@gmail.com>
2023-09-16 17:18:23 +0300

    add static generating parameters method and parametrized test method to PropertiesUtilTest

--- src/test/java/com/dmdev/util/PropertiesUtilTest.java
+++ src/test/java/com/dmdev/util/PropertiesUtilTest.java
@@ -1,7 +1,30 @@
 package com.dmdev.util;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PropertiesUtilTest {
+
+    @ParameterizedTest
+    @MethodSource("getPropertyArguments")
+    void checkGet(String key, String expectedValue) {
+        String actualValue = PropertiesUtil.get(key);
+        assertEquals(expectedValue,actualValue);
+    }
+    
+    static Stream<Arguments> getPropertyArguments() {
+
+        return Stream.of(
+                Arguments.of("db.url", "jdbc:postgresql://localhost:5432/flight_repository"),
+                Arguments.of("db.user", "postgres"),
+                Arguments.of("db.password", "pass"),
+                Arguments.of("db.driver", "org.postgresql.Driver")
+        );
+    }
 
 }
