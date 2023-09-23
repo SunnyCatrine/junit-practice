6a9efbbde4aa9e10d717d0022a8a556b559b2a9c
Catherine <sunny4evil@gmail.com>
2023-09-23 18:30:02 +0300

    correct 3nie kavichki spasibo Denis

--- src/test/java/com/dmdev/integration/IntegrationTestBase.java
+++ src/test/java/com/dmdev/integration/IntegrationTestBase.java
@@ -9,18 +9,7 @@ import java.sql.SQLException;
 public abstract class IntegrationTestBase {
 
     private static final String CLEAN_SQL = "DELETE FROM users;";
-    private static final String CREATE_SQL = """
-            CREATE TABLE IF NOT EXISTS users
-            (
-                id INT AUTO_INCREMENT PRIMARY KEY ,
-                name VARCHAR(64) NOT NULL,
-                birthday DATE NOT NULL ,
-                email VARCHAR(64) NOT NULL UNIQUE ,
-                password VARCHAR(64) NOT NULL ,
-                role VARCHAR(32) NOT NULL ,
-                gender VARCHAR(16) NOT NULL
-            );
-            """;
+    private static final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(64) NOT NULL, birthday DATE NOT NULL,email VARCHAR(64) NOT NULL UNIQUE,password VARCHAR(64) NOT NULL,role VARCHAR(32) NOT NULL,gender VARCHAR(16) NOT NULL);";
 
     @BeforeAll
     static void prepareDatabase() throws SQLException {
